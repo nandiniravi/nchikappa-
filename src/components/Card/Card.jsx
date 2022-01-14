004aeec9fe6d253876f309432cec3f96e79f49a5
Nandini Ravi <nandiniravi1112@gmail.com>
2022-01-14 23:30:38 +0530

    Updates made to links

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,16 @@ import './Card.scss';
 import {Link} from 'react-router-dom';
 
 const Card = (props) => {
+    if(props.title === 'Introduction' || props.title === 'Impression'){
+        return(
+            <a href={props.link} target="_blank">
+                <div className='card'>
+                    <p>{props.title}</p>
+                </div>
+            </a>
+        );
+    }
+
     return(
         <Link to={props.link}>
             <div className='card'>
--- src/components/Commendations/Commendations.jsx
+++ src/components/Commendations/Commendations.jsx
@@ -1,33 +1,18 @@
 import './Commendations.scss';
 import pic from '../../assets/images/wallpaper.jpg';
+import data from '../constants'
 
-const data = [
-    { by : 'Mr. Ravi',
-    relation: 'Brother',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    { by : 'Mr. Abc',
-    relation: 'Friend',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    { by : 'Mr. Pqr',
-    relation: 'Co-worker',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    { by : 'Mr. Xyz',
-    relation: 'Friend',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    { by : 'Mr. Def',
-    relation: 'Co-worker',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-];
+const commendationsDetails = data.commendationsList;
 
 const Commendations = () => {
     return(
         <div className='commendations-main'>
-            <h2>His Commendations</h2>
-            {data.map(each => {
+            <h2>Commendations</h2>
+            {commendationsDetails.map(each => {
                 return(
-                    <div className='commendations-sub' >
+                    <a href={each.link} target="_blank"><div className='commendations-sub'>
                      {each.by}, {each.relation}
-                    </div>
+                    </div></a>
                      
                 );
             })}
